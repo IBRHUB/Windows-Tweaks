bb7dfc6a2fbd479ef4c1d1fca9bc2ae460a9cc73
IBRAHIM <102235503+ibrpride@users.noreply.github.com>
2024-07-19 06:40:38 +0300

    Update Disabling Unnecessary Services.bat

--- 6 - Reduce Processes/Disabling Unnecessary Services.bat
+++ 6 - Reduce Processes/Disabling Unnecessary Services.bat
@@ -2,11 +2,6 @@
 
 :: Enable Delayed Expansion
 setlocal enabledelayedexpansion
-    cd /d %~dp0 
-whoami /user | find /i "S-1-5-18" > nul 2>&1 || (
-    call "RunAsTI.cmd" "%~f0" %*
-    exit /b
-)
 
 cd /d %~dp0 
 start Disclaimer.txt
